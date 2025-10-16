28b05208af9258ecf22b93f351d5629761f3fcb8
Julian Santanatoglia <juliansantanatoglia@gmail.com>
2025-10-16 16:35:59 +0200

    fix: deploy netlify

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,17 +1,17 @@
 // Import all images as modules so Vite can process them
-import paso1 from '/public/images/paso1.png';
-import paso2 from '/public/images/paso2.png';
-import paso3 from '/public/images/paso3.png';
-import paso4 from '/public/images/paso4.png';
-import paso5 from '/public/images/paso5.png';
-import paso6 from '/public/images/paso6.png';
-import placeholder from '/public/images/placeholder.svg';
+import paso1 from '/images/paso1.png';
+import paso2 from '/images/paso2.png';
+import paso3 from '/images/paso3.png';
+import paso4 from '/images/paso4.png';
+import paso5 from '/images/paso5.png';
+import paso6 from '/images/paso6.png';
+import placeholder from '/images/placeholder.svg';
 
 // Project images
-import mushroom from '/public/images/proyectos/1mushroommdp.png';
-import joyeria from '/public/images/proyectos/3joyeriadiaz.png';
-import automatizaciones from '/public/images/proyectos/make.png';
-import inmobiliaria from '/public/images/proyectos/inmobiliaria.jpg';
+import mushroom from '/images/proyectos/1mushroommdp.png';
+import joyeria from '/images/proyectos/3joyeriadiaz.png';
+import automatizaciones from '/images/proyectos/make.png';
+import inmobiliaria from '/images/proyectos/inmobiliaria.jpg';
 
 export const images = {
   steps: {
